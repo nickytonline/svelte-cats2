c361a7d9792b3693f960f15085270a3cddb11322
Nick Taylor <nick@iamdeveloper.com>
2022-08-10 13:41:18 -0400

    having fun with routes and params

---
+++ src/routes/test-integer/[index=integer].svelte
@@ -0,0 +1,8 @@
+<script>
+  import { page } from '$app/stores';
+  console.log($page.params)
+  const { index: pageNumber} = $page.params
+</script>
+
+<h1>You are in test integer page {pageNumber}</h1>
+YOLO
